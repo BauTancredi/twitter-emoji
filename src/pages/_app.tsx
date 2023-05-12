98d24a5769a324438cef6934a3c08b2f336a6d16
bautancredi <bautistatancredi@hotmail.com>
2023-05-12 14:08:09 -0300

    Fetching user data and displaying it

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
 
 import { api } from "~/utils/api";
 
@@ -9,6 +10,11 @@ import "~/styles/globals.css";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Twitter Emoji</title>
+        <meta content="💭" name="description" />
+        <link href="/favicon.ico" rel="icon" />
+      </Head>
       <Toaster position="bottom-right" />
       <Component {...pageProps} />
     </ClerkProvider>
